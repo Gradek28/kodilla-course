aafd005068c50785d7c35958919a1106ded516d0
Grzegorz Gradowski <gr.gradowski@gmail.com>
2023-03-07 22:20:36 +0100

    b

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,17 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String myName = "Grzegorz";
+        int myAge = 28;
+        double height = 1.78;
+        char myFirstLetterName = 'G';
+        boolean isGoodProgrammer = false;
+        byte myLuckyNumber = 7;
+        System.out.println("wiek: " + myAge );
+        System.out.println("imię:" + myName);
+        System.out.println("wzrost:" + height);
+        System.out.println("pierwsza litera imienia:" + myFirstLetterName);
+        System.out.println("dobry programista:" + isGoodProgrammer);
+        System.out.println("szczęśliwa liczba:" + myLuckyNumber);
+
+    }
+}
