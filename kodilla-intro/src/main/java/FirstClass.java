d5df06b24a0200122095132fdf388241d50493a7
Grzegorz Gradowski <gr.gradowski@gmail.com>
2023-03-11 13:11:10 +0100

    Notebook.java

---
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -0,0 +1,27 @@
+public class FirstClass {
+    public static void main(String[] args) {
+        Notebook notebook = new Notebook(600 , 700 , 2020 );
+        System.out.println(notebook.weight + " " + notebook.price + " " + notebook.year);
+        notebook.checkPrice();
+        notebook.checkWeight();
+        notebook.checkYear();
+
+        Notebook heavyNotebook = new Notebook(2000 , 3000 , 2021);
+        System.out.println(heavyNotebook.weight + " " + heavyNotebook.price + " " + heavyNotebook.year);
+        heavyNotebook.checkPrice();
+        heavyNotebook.checkWeight();
+        heavyNotebook.checkYear();
+
+        Notebook oldNotebook = new Notebook(1200 , 400 , 2012);
+        System.out.println(oldNotebook.weight + " " + oldNotebook.price + " " + oldNotebook.year);
+        oldNotebook.checkPrice();
+        oldNotebook.checkWeight();
+        oldNotebook.checkYear();
+
+       }
+
+
+    }
+
+
+
