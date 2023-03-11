cb2662be850a0c7ef0df6aeeb1701c8dc3ee9a3f
Grzegorz Gradowski <gr.gradowski@gmail.com>
2023-03-11 14:56:32 +0100

    Zadanie 2.6

---
+++ kodilla-intro/src/main/java/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+
+    public String name;
+    public double age;
+    public double height;
+
+    public Person(String name,double age, double height){
+    this.name = name;
+    this.age = age;
+    this.height = height;
+        }
+
+    public static void main(String[] args) {
+        Person Adam = new Person("Adam",40.5,178);
+        if (Adam.age > 30 && Adam.height > 160) {
+            System.out.println("User is older than 30 and taller than 160cm");
+        }else{
+            System.out.println("User is 30 (or younger) or 160cm (or shorter)");
+        }
+    }
+
+
+    }
+
+
+
+
