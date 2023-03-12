a905b471f2852620ffbafcf87d3648b2a4fe0993
Grzegorz Gradowski <gr.gradowski@gmail.com>
2023-03-12 16:35:07 +0100

    Zadanie 2.4

---
+++ kodilla-intro/src/main/java/GradesNext.java
@@ -0,0 +1,10 @@
+public class GradesNext {
+    public static void main(String[] args) {
+        Grades grades = new Grades();
+        grades.add(4);
+        grades.add(5);
+        grades.add(2);
+        System.out.println(grades.lastGrade());
+        System.out.println(grades.averageGrades());
+    }
+}
