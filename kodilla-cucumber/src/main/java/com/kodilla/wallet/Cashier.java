16c4b206a112bca1141cae2e2963eb0ed80be1ed
Grzegorz Gradowski <gr.gradowski@gmail.com>
2023-06-03 13:21:07 +0200

    cucumber

---
+++ kodilla-cucumber/src/main/java/com/kodilla/wallet/Cashier.java
@@ -0,0 +1,28 @@
+package wallet;
+
+import com.kodilla.wallet.CashSlot;
+import com.kodilla.wallet.Wallet;
+
+public class Cashier {
+    private final CashSlot cashSlot;
+
+    public Cashier(CashSlot cashSlot) {
+        this.cashSlot = cashSlot;
+    }
+
+    public void withdraw(Wallet wallet, int amount) {
+        if (wallet.getBalance() >= amount) {
+            cashSlot.dispense(amount);
+        } else {
+            cashSlot.dispense(0);
+        }
+    }
+
+    public String messageFromCashier(Wallet wallet, int amount) {
+        if (wallet.getBalance() >= amount) {
+            return "Here you are";
+        } else {
+            return "You don't have enough money in your wallet";
+        }
+    }
+}
---
+++ kodilla-cucumber/src/test/java/wallet/WalletSteps.java
@@ -0,0 +1,29 @@
+package wallet;
+
+import com.kodilla.wallet.CashSlot;
+
+import com.kodilla.wallet.Wallet;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+
+public class WalletSteps {
+    private final Wallet wallet = new Wallet();
+    private final CashSlot cashSlot = new CashSlot();
+
+    @Given("I have deposited ${int} in my wallet")
+    public void iHaveDeposited$Money_to_walletInMyWallet(int int1) {
+        wallet.deposit(int1);
+        Assert.assertEquals(int1, wallet.getBalance());
+    }
+    @When("I request ${int}")
+    public void iRequest$Money_from_wallet(int money_from_wallet) {
+        wallet.Cashier cashier = new wallet.Cashier(cashSlot);
+        cashier.withdraw(wallet, money_from_wallet);
+    }
+    @Then("${int} should be dispensed")
+    public void $Money_dispensedShouldBeDispensed(int money_dispensed) {
+        Assert.assertEquals(money_dispensed, cashSlot.getContents());
+    }
+}
