ad9a0e5f8fa19e0d6de16f49acd4bed2957177fa
Grzegorz Gradowski <gr.gradowski@gmail.com>
2023-06-09 18:04:27 +0200

    code-coverage

---
+++ kodilla-code-coverage/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
@@ -0,0 +1,126 @@
+package com.kodilla.jdbc;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DbManagerTestSuite {
+    private static DbManager dbManager;
+
+    //
+    @BeforeAll
+    public static void setup() throws SQLException {
+        dbManager = DbManager.getInstance();
+    }
+
+    @Test
+    void testSelectUsers() throws SQLException {
+        //Given
+        String countQuery = "SELECT COUNT(*) FROM USERS";
+        Statement statement = createStatement();
+        ResultSet rs = statement.executeQuery(countQuery);
+        int count = getRowsCount(rs);
+        insertUsers(statement);
+
+        //When
+        String sqlQuery = "SELECT * FROM USERS";
+        statement = createStatement();
+        rs = statement.executeQuery(sqlQuery);
+
+        //Then
+        int counter = getResultsCount(rs);
+        int expected = count + 5;
+        assertEquals(expected, counter);
+
+        rs.close();
+        statement.close();
+    }
+
+    @Test
+    void testSelectUsersAndPosts() throws SQLException {
+        //given
+        String countQuery = "SELECT U.FIRSTNAME, U.LASTNAME\n" +
+                "FROM USERS U\n" +
+                "JOIN POSTS P ON U.ID = P.USER_ID\n" +
+                "GROUP BY U.ID, U.FIRSTNAME, U.LASTNAME\n" +
+                "HAVING COUNT(P.ID) >= 2;";
+        Statement statement = createStatement();
+        ResultSet rs = statement.executeQuery(countQuery);
+        int counter = 0;
+        while (rs.next()) {
+            System.out.println(rs.getString("FIRSTNAME") + " " +
+                    rs.getString("LASTNAME"));
+            counter++;
+        }
+        ResultSet actualResultSet = statement.executeQuery(countQuery);
+        List<String> expectedUserList = Arrays.asList("Marcin Najman", "Jacek Murański");
+
+        int actualCount = 0;
+        List<String> actualUserList = new ArrayList<>();
+        while (actualResultSet.next()) {
+            String fullName = actualResultSet.getString("FIRSTNAME") + " " +
+                    actualResultSet.getString("LASTNAME");
+            actualUserList.add(fullName);
+            actualCount++;
+        }
+        //then
+        assertEquals(expectedUserList, actualUserList);
+        assertEquals(expectedUserList.size(), actualCount);
+
+        rs.close();
+        statement.close();
+    }
+
+    private Statement createStatement() throws SQLException {
+        return dbManager.getConnection().createStatement();
+    }
+
+    private static final List<AbstractMap.SimpleEntry<String, String>> USERS = List.of(
+            new AbstractMap.SimpleEntry<>("Zara", "Ali"),
+            new AbstractMap.SimpleEntry<>("Otman", "Use"),
+            new AbstractMap.SimpleEntry<>("Mark", "Boq"),
+            new AbstractMap.SimpleEntry<>("Uli", "Wimer"),
+            new AbstractMap.SimpleEntry<>("Oli", "Kosiw")
+    );
+
+    private void insertUsers(Statement statement) throws SQLException {
+        for (AbstractMap.SimpleEntry<String, String> user : USERS) {
+            statement.executeUpdate(
+                    String.format("INSERT INTO USERS(FIRSTNAME, LASTNAME) VALUES ('%s', '%s')",
+                            user.getKey(),
+                            user.getValue()
+                    )
+            );
+        }
+    }
+
+    private static int getResultsCount(ResultSet rs) throws SQLException {
+        int counter = 0;
+        while (rs.next()) {
+            System.out.printf("%d, %s, %s%n",
+                    rs.getInt("ID"),
+                    rs.getString("FIRSTNAME"),
+                    rs.getString("LASTNAME"));
+            counter++;
+        }
+        return counter;
+    }
+
+    private static int getRowsCount(ResultSet rs) throws SQLException {
+        int count = 0;
+        while (rs.next()) {
+            count = rs.getInt("COUNT(*)");
+        }
+        return count;
+    }
+}
